a3ef262e8d88aa9968a52c728d1b099619ae6336
Sebastian <sgniffke@web.de>
2021-10-05 13:05:12 +0200

    Grab fix

--- Assets/Scripts/GraveHandler.cs
+++ Assets/Scripts/GraveHandler.cs
@@ -6,17 +6,29 @@ using UnityEngine;
 public class GraveHandler : MonoBehaviour
 {
     public GameObject hole;
+    public static void aktiv = false;
+
+
     public void handleInteraction(Item item, Item lastUsed)
     {
         if (lastUsed.itemName == "Schaufel" && FindObjectOfType<Inventory>().searchItem("foot_bones") == false)
         {
             FindObjectOfType<Inventory>().addItem(ItemDatabaseInstance.getItemByName("foot_bones"));
             hole.SetActive(true);
+            aktiv = true;
         }
     }
 
     public void handleInteractionNoItem()
     {
         FindObjectOfType<Dialog>().showText("Wenn ich nur einen Riesenlöffel hätte.");
+    }
+
+    public void Start()
+    {
+        if(aktiv)
+        {
+            hole.SetActive(true);
+        }
     }
 }
